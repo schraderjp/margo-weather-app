b49d234525dd0f0ac4dbf61548ca5d75bed72df2
schraderjp <schraderjp@gmail.com>
2022-09-30 15:52:59 -0400

    added loading spinner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import { NextPage } from 'next';
-import { Container, Heading, Text } from '@chakra-ui/react';
+import { Container, Flex, Heading, Text } from '@chakra-ui/react';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import WeatherDisplay from '../components/WeatherDisplay';
 import { format } from 'date-fns';
+import { Spinner } from '@chakra-ui/react';
 
 const Home: NextPage = () => {
   const fetcher = async () => {
@@ -18,7 +19,16 @@ const Home: NextPage = () => {
       <Heading textAlign={'center'} as="h1" size="xl" py={'2'}>
         Weather in Margo, VA
       </Heading>
-      {status === 'loading' && <p>Loading...</p>}
+      {status === 'loading' && (
+        <Flex
+          width={'full'}
+          alignItems="center"
+          justifyContent={'center'}
+          pt="4"
+        >
+          <Spinner />
+        </Flex>
+      )}
       {status === 'success' && (
         <>
           <Text py="2" textAlign={'center'}>
